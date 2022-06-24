46268991f72e23ce541e8978a52db3f7d0787177
unknown <vinhtrungngo1907@gmail.com>
2022-06-24 14:59:41 +0700

    new change

--- server/app.js
+++ server/app.js
@@ -22,7 +22,6 @@ const connectDB = async() => {
 }
 
 // connectDB();
-
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
@@ -33,39 +32,19 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 const client = mqtt.connect('mqtt://broker.mqttdashboard.com');
-// const client = new mqtt.MqttClient(1883, 'mqtt://broker.mqttdashboard.com', 'pusher')
-// client.on("connect", function() {
-//     console.log("Client subscribed ");
-//     client.subscribe("sensor/update");
-// });
 
-// let users = []
 io.on("connection", (socket) => {
     socket.on("joinSocket", (data) => {
-        // users.push({ client: data, socketId: socket.id })
         socket.join(data.topic);
         console.log(`User with ID: ${socket.id} joined with topic: ${data.topic}`);
         client.subscribe(data.topic);
     })
 
     client.addListener('message', function(topic, payload) {
-        // sys.puts(topic+'='+payload);
         console.log(payload.toString())
         io.sockets.in(topic).emit('mqttData', JSON.parse(payload.toString()));
     });
-    // client.on("message", function(topic, message) {
-    //     console.log(topic)
-    //     if (topic === "sensor/update") {
-    //         let newMess = JSON.parse(message.toString());
-    //         console.log(newMess)
-    //             // socket.emit("mqttData", newMess)
-    //             // socket.emit("sensor/updateToClient", newMess)
-    //             // io.emit("sensor/updateToClient", newMess);
-    //             // socket.broadcast.emit("sensor/updateToClient", newMess)
-    //             // socket.to(`Socket ${socket.id}`).emit("sensor/updateToClient", newMess)
-    //         client.end();
-    //     }
-    // })
+
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
     })
